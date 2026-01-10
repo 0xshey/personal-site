8fd58d3dff2918f425c8c82f3dfcf66b5494c40e
Shey Laplanche <sheylaplanche@gmail.com>
2026-01-10 12:04:25 -0800

    New style

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -6,6 +6,11 @@ import { useTheme } from "next-themes";
 
 export function ThemeToggle() {
 	const { setTheme, resolvedTheme } = useTheme();
+	const [mounted, setMounted] = React.useState(false);
+
+	React.useEffect(() => {
+		setMounted(true);
+	}, []);
 
 	function toggleTheme() {
 		setTheme((oldTheme) => {
@@ -15,15 +20,29 @@ export function ThemeToggle() {
 		});
 	}
 
+	if (!mounted) {
+		return (
+			<div className="h-full w-full relative flex place-items-center justify-center">
+				<p className="text-sm text-muted-foreground opacity-0">
+					Loading
+				</p>
+			</div>
+		);
+	}
+
 	return (
 		<button
 			onClick={toggleTheme}
 			className="h-full w-full relative flex place-items-center justify-center cursor-pointer"
 		>
 			{resolvedTheme === "dark" ? (
-				<p className="text-sm text-muted-foreground hover:text-foreground transition-colors duration-200">Dark</p>
+				<p className="text-sm text-muted-foreground hover:text-foreground transition-colors duration-200">
+					Dark
+				</p>
 			) : (
-				<p className="text-sm text-muted-foreground hover:text-foreground transition-colors duration-200">Light</p>
+				<p className="text-sm text-muted-foreground hover:text-foreground transition-colors duration-200">
+					Light
+				</p>
 			)}
 			<span className="sr-only">Toggle theme</span>
 		</button>
